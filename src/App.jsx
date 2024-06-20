74afd7ee807ff850a47a91f04d68db5bc79624bf
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2024-06-20 21:12:41 +1000

    Finish Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css';
 import { SignUpForm } from './SignUpForm';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-//import { Login } from './Login';
+import { Login } from './Login';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
     <Route path="/signup" element={<SignUpForm />} />
-    {/*
-    
-     */}
+    <Route path="/login" element={<Login />} />
     </Routes>
     </BrowserRouter>
   )
